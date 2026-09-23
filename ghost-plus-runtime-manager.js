0a7915e94301de7984309d2592a562ad54f810a3
tuanvayt03-cpu <tuanva.yt03@gmail.com>
2026-09-23 11:51:06 +0700

    release: add safe 25-minute continuity lease

--- ghost-plus-runtime-manager.js
+++ ghost-plus-runtime-manager.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const ROOT='__ghostPlusRuntime';
-const VERSION='0.15.2';
+const VERSION='0.15.3';
 const previous=window[ROOT];
 try { if(previous?.active && typeof previous.destroy==='function') previous.destroy('reinject'); } catch (_) {}
 
